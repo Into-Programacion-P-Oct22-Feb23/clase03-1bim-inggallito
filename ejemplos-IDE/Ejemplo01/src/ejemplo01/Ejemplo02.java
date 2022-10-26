653f6dd1a4564066f944969fd2cf83032338bcb8
unknown <mspineda3@utpl.edu.ec>
2022-10-26 15:50:03 -0500

    modificado

---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/Ejemplo02.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ejemplo02 {
+        public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo, soy Daniel Ojeda estudiante de computación en la UTPL");
+    }
+}
